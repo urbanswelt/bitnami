c7c9ed5b37d7fa3e28e05316d1e7356f2d0e92a4
Lars Urban <urbanswelt@gmail.com>
2021-02-09 14:05:25 +0100

    Create bitnami-ssh-setup.sh

---
+++ bitnami-ssh-setup.sh
@@ -0,0 +1,10 @@
+#https://www.ssh-audit.com/hardening_guides.html
+
+sudo su
+#Re-generate the RSA and ED25519 keys
+rm /etc/ssh/ssh_host_*
+ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ""
+ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ""
+#Remove small Diffie-Hellman moduli
+awk '$5 >= 3071' /etc/ssh/moduli > /etc/ssh/moduli.safe
+mv /etc/ssh/moduli.safe /etc/ssh/moduli
