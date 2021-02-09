42dbeab05e091918c868a1c57c887354c1c86b0f
Lars Urban <urbanswelt@gmail.com>
2021-02-09 22:45:54 +0100

    Update bitnami-postgresql.sh

--- bitnami-postgresql.sh
+++ bitnami-postgresql.sh
@@ -6,6 +6,8 @@ sudo apt update && sudo apt upgrade --yes
 #enable ssh
 wget https://raw.githubusercontent.com/urbanswelt/bitnami/main/bitnami-ssh-setup.sh
 bash bitnami-ssh-setup.sh
+#ssh audit overview
+read -p "Press [Enter] key to continue..."
 
 #dns setup - restart script !!!
 wget https://raw.githubusercontent.com/urbanswelt/bitnami/main/bitnami-local_dns-setup.sh
